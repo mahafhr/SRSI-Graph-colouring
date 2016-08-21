1076379c10b30be09758dbd7e4d0b5fbe4fc92ae
HexTree <lamencel@gmail.com>
2016-08-22 02:47:11 +0300

    adding coloured dots to pygame

--- main.py
+++ main.py
@@ -7,15 +7,27 @@ width = 800
 height = 600
 
 dot_width = 50
+dot_radius = 16
+
+coordinates = dict()
+
+colour_map = {0: (0, 0, 0), 1: (50, 70, 255), 2: (255, 50, 50), 3: (50, 255, 80), 4: (255, 255, 50)}
 
 def dist_squared(pair1, pair2):
     return (pair1[0] - pair2[0])**2 + (pair1[1] - pair2[1])**2
 
 
-def nearest_vertex(click, coordinates):
+def nearest_vertex(click):
     # finds the vertex closest to click
+    global coordinates
     return min(range(1, n+1), key = lambda v: dist_squared(click, coordinates[v]))
 
+def colour_nodes(g):
+    global coordinates
+    for i in range(1, g.order()+1):
+        if g.vertex_colour[i] in colour_map:
+            pygame.draw.circle(screen, colour_map[g.vertex_colour[i]], coordinates[i], dot_radius)
+    pygame.display.flip()
 
 # Setup pygame screen
 pygame.init()
@@ -42,8 +54,8 @@ pygame.display.flip()
 # empty = pygame.transform.smoothscale(empty, (width/m, height/n))
 
 phase = 1 # phase 1 is building the graph with left clicks, phase 2 is connecting the edges with right clicks
+# phase 3 colours, phase 4 exits
 n = 0
-coordinates = dict()
 
 # This loop waits for mouse clicks and handles them
 done = False
@@ -62,31 +74,34 @@ while not done:
             if phase == 2:
                 g = Graph(n)
             elif phase == 3:
+                # Choose colouring algorithm here
+                g.colour_greedy("h")
+
+                g.print_graph()
+                colour_nodes(g)
+            elif phase == 4:
                 done = True
         elif event.type == MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0] and phase == 1:
                 n += 1
                 pos = pygame.mouse.get_pos()
                 coordinates[n] = pos
-                # draw dot
-                pos_offset = (pos[0] - dot_width/2, pos[1] - dot_width/2)
-                screen.blit(dot, pos_offset)
+                # pos_offset = (pos[0] - dot_width/2, pos[1] - dot_width/2)
+                # screen.blit(dot, pos_offset)
+                pygame.draw.circle(screen, (0, 0, 0), pos, dot_radius)
                 pygame.display.flip()
-            elif pygame.mouse.get_pressed()[2]:
+            elif pygame.mouse.get_pressed()[2] and phase == 2:
                 if first_right_click:
-                    temp_vertex = nearest_vertex(pygame.mouse.get_pos(), coordinates)
+                    temp_vertex = nearest_vertex(pygame.mouse.get_pos())
                     first_right_click = False
                 else:
                     pos = pygame.mouse.get_pos()
-                    temp = nearest_vertex(pos, coordinates)
+                    temp = nearest_vertex(pos)
                     g.add_edge(temp_vertex, temp)
                     first_right_click = True
                     #draw line
-                    pygame.draw.line(screen, (0, 0, 0), coordinates[temp_vertex], coordinates[temp])
+                    pygame.draw.line(screen, colour_map[0], coordinates[temp_vertex], coordinates[temp])
                     pygame.display.flip()
-
-
-g.print_graph()
 
 
 print "Program terminated."
